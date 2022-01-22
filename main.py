b1ad2d465ee447fa54ec95b83eebd12784c354e2
CW <cwainwright26@btinternet.com>
2022-01-22 23:39:20 +0000

    Sorted imports, added Docstring, added call to save_notebook function after each change

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from os import name, system
 
-from commands import build_notebook, display_commands, display_notebook
+from commands import (
+    build_notebook,
+    display_commands,
+    display_notebook,
+    save_notebook
+)
+
 
 def main():
+    """"Main Process"""
     system('cls' if name == 'nt' else 'clear')
     root = build_notebook()
     current_note = None
@@ -12,6 +19,7 @@ def main():
             current_note = root
         display_notebook(root)
         current_note = display_commands(current_note)
+        save_notebook(root)
         
 if __name__ == "__main__":
     main()
