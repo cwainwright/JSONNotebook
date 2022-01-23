4d6f4ab16b78196f5d55d24f7923ccb05465b014
CW <cwainwright26@btinternet.com>
2022-01-23 11:00:21 +0000

    Fixed AttributeError where .values() was called on children list in node_display function

--- toolset.py
+++ toolset.py
@@ -65,7 +65,7 @@ def node_display(node, indent = 0) -> str:
         display_string = str(node)
     else:
         display_string = "\n" + indent * " " + "\u2514" + str(node)
-    for child in node.children.values():
+    for child in node.children:
         display_string += node_display(child, indent + 1)
 
     return display_string
