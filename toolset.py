8226f3cadaecb061341be3c1e471ec1f3189f932
CW <cwainwright26@btinternet.com>
2022-01-23 10:19:34 +0000

    Added Node class and clear_screen function

--- toolset.py
+++ toolset.py
@@ -1,5 +1,63 @@
 from typing import Union
+from os import system, name
 
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.children = []
+        self.parent = None
+    
+    @property
+    def childless(self) -> bool:
+        return not bool(len(self.children))
+    
+    @property
+    def path(self) -> str:
+        if self.parent is None:
+            return self.data
+        else:
+            return self.parent.path + "/" + self.data
+
+    @property
+    def json(self) -> dict:
+        data = {"title": self.data, "content": self.content, "tags": self.tags, "children": []}
+        for child in self.children:
+            data.get("children").append(child.json)
+        return data
+        
+    def __str__(self) -> str:
+        return str(self.data)
+    
+    def __repr__(self) -> str:
+        return str(self.path)
+    
+    def node_display(self, indent = 0) -> str:
+        if not indent:
+            display_string = str(self)
+        else:
+            display_string = "\n" + indent * " " + "\u2514" + str(self)
+        for child in self.children:
+            display_string += child.node_display(indent + 1)
+        return display_string
+    
+    def set_parent(self, parent) -> bool:
+        self.parent = parent
+        return True
+    
+    def add_child(self, child) -> bool:
+        if child in self.children:
+            return False
+        child.set_parent(self)
+        self.children.append(child)
+        return True
+        
+    def remove_child(self, child):
+        try:
+            self.children.remove(child)
+        except KeyError:
+            return False
+        else:
+            return True
 
 def options_menu(menu: Union[list, dict], arguments = None):
     option_list = []
@@ -34,7 +92,9 @@ def options_menu(menu: Union[list, dict], arguments = None):
         except TypeError:
             return value
 
-
+def clear_screen():
+    """Clear screen for Windows and Linux"""
+    system('cls' if name == 'nt' else 'clear')
 
 if __name__ == "__main__":
     
