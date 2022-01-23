e25187469db309bb1ed54874989d43d8e75baace
CW <cwainwright26@btinternet.com>
2022-01-23 10:57:48 +0000

    Moved node_display function to toolset, deleted formatters.py

--- toolset.py
+++ toolset.py
@@ -59,6 +59,17 @@ class Node:
         else:
             return True
 
+def node_display(node, indent = 0) -> str:
+    """Display node and its children"""
+    if not indent:
+        display_string = str(node)
+    else:
+        display_string = "\n" + indent * " " + "\u2514" + str(node)
+    for child in node.children.values():
+        display_string += node_display(child, indent + 1)
+
+    return display_string
+
 def options_menu(menu: Union[list, dict], arguments = None):
     option_list = []
     selection = None
